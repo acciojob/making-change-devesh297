993b0199ea13d8cf17c552d8b2656b3d3b344821
Devesh Goswami <53081430+devesh297@users.noreply.github.com>
2025-03-22 09:18:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+  let change = {};
+
+  change.q = Math.floor(amount / 25); // Number of quarters
+  amount %= 25; // Remaining cents after quarters
+
+  change.d = Math.floor(amount / 10); // Number of dimes
+  amount %= 10; // Remaining cents after dimes
+
+  change.n = Math.floor(amount / 5); // Number of nickels
+  amount %= 5; // Remaining cents after nickels
+
+  change.p = amount; // Remaining pennies
+
+  return change;
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
